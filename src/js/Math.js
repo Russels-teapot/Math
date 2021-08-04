8b49656e68444cf259fc7b0e9fd020831140e0b1
Russels-teapot <baxarev.777@yandex.ru>
2021-08-04 18:31:40 +0300

    Выполнение домашней работы

--- src/js/Math.js
+++ src/js/Math.js
@@ -1,21 +1,21 @@
 export default class MathCharacter {
   getAttack(cellNumber) {
     if (cellNumber <= 5) {
-      this.attack -= this.attack * ((cellNumber - 1) / 10);
+      this.modAttack = this.attack - this.attack * ((cellNumber - 1) / 10);
     }
     if (cellNumber > 5) {
-      this.attack = 0;
+      this.modAttack = 0;
     }
-    return this.attack;
+    return this.modAttack;
   }
 
   getStoned(cellNumber) {
     if (cellNumber <= 5) {
-      this.attack -= Math.round(Math.log2(cellNumber)) * 5;
+      this.modAttack -= Math.round(Math.log2(cellNumber)) * 5;
     }
     if (cellNumber > 5) {
-      this.attack = 0;
+      this.modAttack = 0;
     }
-    return this.attack;
+    return this.modAttack;
   }
 }
--- src/js/__tests__/Math.test.js
+++ src/js/__tests__/Math.test.js
@@ -6,33 +6,33 @@ const magician = new Magician('Alex', 'Magician');
 
 test('two point attack', () => {
   daemon.getAttack(2);
-  expect(daemon.attack).toBe(90);
+  expect(daemon.modAttack).toBe(90);
 });
 
 test('five point attack', () => {
   magician.getAttack(5);
-  expect(magician.attack).toBe(60);
+  expect(magician.modAttack).toBe(60);
 });
 
 test('six point attack', () => {
   magician.getAttack(6);
-  expect(magician.attack).toBe(0);
+  expect(magician.modAttack).toBe(0);
 });
 
 test('two point get stoned', () => {
   daemon.getAttack(2);
   daemon.getStoned(2);
-  expect(daemon.attack).toBe(85);
+  expect(daemon.modAttack).toBe(85);
 });
 
 test('five point get stoned', () => {
   magician.getAttack(5);
   magician.getStoned(5);
-  expect(magician.attack).toBe(50);
+  expect(magician.modAttack).toBe(50);
 });
 
 test('six point get stoned', () => {
   daemon.getAttack(6);
   daemon.getStoned(6);
-  expect(daemon.attack).toBe(0);
+  expect(daemon.modAttack).toBe(0);
 });
